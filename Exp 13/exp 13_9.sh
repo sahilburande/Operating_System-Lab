b96d5d2f1ed263602af8267c6c669035fc9886c3
Sahil Burande <sahilburande@gmail.com>
2026-04-16 13:54:21 +0530

    Create exp 13_9.sh

---
+++ Exp 13/exp 13_9.sh
@@ -0,0 +1,12 @@
+echo "Enter a year:"
+read year
+
+if [ $((year % 400)) -eq 0 ]; then
+    echo "$year is a leap year"
+elif [ $((year % 100)) -eq 0 ]; then
+    echo "$year is not a leap year"
+elif [ $((year % 4)) -eq 0 ]; then
+    echo "$year is a leap year"
+else
+    echo "$year is not a leap year"
+fi
